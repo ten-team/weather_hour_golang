9d927348a91e854fe7ba0df8e49c7ec85ba8aade
yoshitake <oed0cow6oy5@gmail.com>
2017-11-13 02:10:07 +0900

    Implement LED ON/OFF

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"github.com/jgarff/rpi_ws281x/golang/ws2811"
+	"log"
+	"os"
+	"time"
+)
+
+func color(red uint8, green uint8, blue uint8) uint32 {
+	return uint32(red)<<8 + uint32(green)<<16 + uint32(blue)
+}
 
 func main() {
-	fmt.Printf("Hello world\n")
+	log.Printf("Booting...\n")
+	const (
+		GPIO_PIN           = 18
+		LED_COUNT          = 3
+		DEFAULT_BRIGHTNESS = 255
+	)
+	if err := ws2811.Init(GPIO_PIN, LED_COUNT, DEFAULT_BRIGHTNESS); err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	for {
+		ws2811.SetLed(0, color(0, 0, 0xFF))
+		ws2811.SetBrightness(1, 255)
+		if err := ws2811.Render(); err != nil {
+			log.Println(err)
+		}
+		time.Sleep(time.Second)
+
+		ws2811.SetLed(0, color(0, 0, 0xFF))
+		ws2811.SetBrightness(1, 128)
+		if err := ws2811.Render(); err != nil {
+			log.Println(err)
+		}
+		time.Sleep(time.Second)
+
+		ws2811.SetLed(0, 0)
+		ws2811.SetBrightness(1, 255)
+		if err := ws2811.Render(); err != nil {
+			log.Println(err)
+		}
+		time.Sleep(time.Second)
+	}
 }
